f8d3b6974a64b6a1e17670cd0f902f527afecb1e
d33d33 <kevin@d33d33.fr>
2017-01-03 13:55:44 -0500

    feat: add version command

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "1.0.0"
+	githash = "HEAD"
+)
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("haproxy-exporter %s (%s)\n", version, githash)
+	},
+}
